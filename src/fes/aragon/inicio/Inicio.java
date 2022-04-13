eafaabd67a47bbba6701fb9681c562e040a52fa6
Adrian Valdivia <vadrianenrique@gmail.com>
2022-04-13 17:56:24 -0500

    Commit de lectura de caracteres 12/04/2022

--- src/fes/aragon/inicio/Inicio.java
+++ src/fes/aragon/inicio/Inicio.java
@@ -22,127 +22,234 @@ import java.util.Stack;
  * @author MASH
  */
 public class Inicio {
-    private boolean error = true;
-    private Tokens tokens = null;
-    private Lexico analizador = null;
-    private static String[][] reglas = new String[4][4];
-    private static Stack<String> pila_de_simbolos = new Stack<>();
-    private static LinkedList<String> cola = new LinkedList<>();
-    
-    public static void main(String[] args) {
-        Inicio ap = new Inicio();
-        
-        
-        //Reglas de producción
-        reglas[0][0] = "S:=AB";
-        reglas[0][1] = "S:=AB";
-        reglas[0][2] = "error";
-        reglas[0][3] = "error";
-        reglas[1][0] = "A:=a";
-        reglas[1][1] = "A:=l";
-        reglas[1][2] = "error";
-        reglas[1][3] = "error";
-        reglas[2][0] = "eror";
-        reglas[2][1] = "B:=bCd";
-        reglas[2][2] = "error";
-        reglas[2][3] = "error";
-        reglas[3][0] = "error";
-        reglas[3][1] = "error";
-        reglas[3][2] = "C:=c";
-        reglas[3][3] = "C:=l";
-        
-        
-        BufferedReader buf;
-        try {
-            buf = new BufferedReader(
-                    new FileReader(System.getProperty("user.dir")
-                            + "/archivo.txt"));
-            
-            
-            ap.analizador = new Lexico(buf);
-            ap.siguienteToken();
-            
-            ap.insertarCola();
-            ap.pilaDeSimbolos();
+	private boolean error = true;
+	private Tokens tokens = null;
+	private Lexico analizador = null;
+	private static String[][] reglas = new String[4][4];
+	private static Stack<String> pila_de_simbolos = new Stack<>();
+	private static LinkedList<String> cola = new LinkedList<>();
+	private static String ultimo_cola = new String();
+	private static String primero_pila = new String();
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ErrorSintactico e) {
-        	e.printStackTrace();
-        }
-    }
-    
-    private void pilaDeSimbolos() throws ErrorSintactico {
-    	pila_de_simbolos.add(";");
-    	pila_de_simbolos.add("S");
-    	String ultimo_cola = cola.peek();
-    	String primero_pila = pila_de_simbolos.peek();
-    	
-    	if (primero_pila == "S" && ultimo_cola == "a") {
-    		reglaS(ultimo_cola, primero_pila);
-    	} else if (primero_pila == "S" && ultimo_cola == "b") {
-    		reglaS(ultimo_cola, primero_pila);
-    	} else if (primero_pila == "S" && ultimo_cola == "c") {
-    		throw new ErrorSintactico("Error sintactico"+ " Linea: " + (tokens.getLinea()+1));
-    	} else if (primero_pila == "S" && ultimo_cola == "c") {
-    		throw new ErrorSintactico("Error sintactico"+ " Linea: " +(tokens.getLinea()+1));
-    	}
-    	
-    	System.out.println(pila_de_simbolos);
-   }
-    
-    private void reglaS(String ultimo_cola , String primero_pila) {
-    	//Eliminamos primer elemento de la pila de simbolos
-		pila_de_simbolos.pop();
-		
-		/*
-		 * De acuerdo a la regla de producción de nuestra tabla
-		 * la sustituimos
-		*/
-		pila_de_simbolos.add("B");
-		pila_de_simbolos.add("A");
-		
-		//Leemos el ultimo de la cola y el primero de la pila
+	public static void main(String[] args) {
+		Inicio ap = new Inicio();
+
+		BufferedReader buf;
+		try {
+			buf = new BufferedReader(new FileReader(System.getProperty("user.dir") + "/archivo.txt"));
+
+			ap.analizador = new Lexico(buf);
+			ap.siguienteToken();
+			ap.insertarCola();
+			ap.pilaDeSimbolos();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ErrorSintactico e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void pilaDeSimbolos() throws ErrorSintactico {
+		pila_de_simbolos.add(";");
+		pila_de_simbolos.add("S");
 		ultimo_cola = cola.peek();
 		primero_pila = pila_de_simbolos.peek();
-    }
-    
-    private void insertarCola() {
-    	if(tokens.getLexema() == Sym.A) {
-    		cola.add("a");
-    		siguienteToken();
-    	}
-    	if(tokens.getLexema() == Sym.B) {
-    		cola.add("b");
-    		siguienteToken();
-    	}
-    	if(tokens.getLexema() == Sym.C) {
-    		cola.add("c");
-    		siguienteToken();
-    	}
-    	if(tokens.getLexema() == Sym.D) {
-    		cola.add("d");
-    		siguienteToken();
-    	}
-    	if(tokens.getLexema() == Sym.PUNTOCOMA) {
-    		cola.add(";");
-    	}
-    }
-    
 
-    
+		while (tokens.getLexema() != Sym.EOF) {
+			while (ultimo_cola != ";" && primero_pila != ";") {
+				if (primero_pila == "S" && ultimo_cola == "a") {
+					reglaS();
+				} else if (primero_pila == "S" && ultimo_cola == "b") {
+					reglaS();
+				} else if (primero_pila == "S" && ultimo_cola == "c") {
+					throw new ErrorSintactico("Error sintactico" + " Linea: " + (tokens.getLinea() + 1));
+				} else if (primero_pila == "S" && ultimo_cola == "d") {
+					throw new ErrorSintactico("Error sintactico" + " Linea: " + (tokens.getLinea() + 1));
+				}
 
-    private void siguienteToken() {
-        try {
-            tokens = analizador.yylex();
-            if (tokens == null) {
-                tokens = new Tokens("EOF", Sym.EOF, 0, 0);
-                throw new IOException("Fin Archivo");
-            }
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
+				if (primero_pila == "A" && ultimo_cola == "a") {
+					reglaA();
+				} else if (primero_pila == "A" && ultimo_cola == "b") {
+					lambda();
+				} else if (primero_pila == "A" && ultimo_cola == "c") {
+					throw new ErrorSintactico("Error sintactico" + " Linea: " + (tokens.getLinea() + 1));
+				} else if (primero_pila == "A" && ultimo_cola == "d") {
+					throw new ErrorSintactico("Error sintactico" + " Linea: " + (tokens.getLinea() + 1));
+				}
 
-    }
+				if (primero_pila == ultimo_cola) {
+					consumir();
+				}
+
+				if (primero_pila == "B" && ultimo_cola == "a") {
+					throw new ErrorSintactico("Error sintactico" + " Linea: " + (tokens.getLinea() + 1));
+				} else if (primero_pila == "B" && ultimo_cola == "b") {
+					reglaB();
+				} else if (primero_pila == "B" && ultimo_cola == "c") {
+					throw new ErrorSintactico("Error sintactico" + " Linea: " + (tokens.getLinea() + 1));
+				} else if (primero_pila == "B" && ultimo_cola == "d") {
+					throw new ErrorSintactico("Error sintactico" + " Linea: " + (tokens.getLinea() + 1));
+				}
+
+				if (primero_pila == "C" && ultimo_cola == "a") {
+					throw new ErrorSintactico("Error sintactico" + " Linea: " + (tokens.getLinea() + 1));
+				} else if (primero_pila == "C" && ultimo_cola == "b") {
+					throw new ErrorSintactico("Error sintactico" + " Linea: " + (tokens.getLinea() + 1));
+				} else if (primero_pila == "C" && ultimo_cola == "c") {
+					reglaC();
+				} else if (primero_pila == "C" && ultimo_cola == "d") {
+					lambda();
+				}
+			}
+
+			System.out.println("linea correcta " + (tokens.getLinea() + 1));
+			siguienteToken();
+
+			// Leemos el ultimo de la cola y el primero de la pila
+			ultimo_cola = cola.peek();
+			primero_pila = pila_de_simbolos.peek();
+
+		}
+	}
+
+	private void reglaS() {
+		System.out.println(pila_de_simbolos);
+		// Eliminamos primer elemento de la pila de simbolos
+		pila_de_simbolos.pop();
+
+		/*
+		 * De acuerdo a la regla de producción de nuestra tabla la sustituimos
+		 */
+		pila_de_simbolos.add("B");
+		pila_de_simbolos.add("A");
+
+		// Leemos el ultimo de la cola y el primero de la pila
+		ultimo_cola = cola.peek();
+		primero_pila = pila_de_simbolos.peek();
+
+		// Imprimimos el cambio
+		System.out.println(pila_de_simbolos);
+	}
+
+	private void reglaA() {
+		// Eliminamos primer elemento de la pila de simbolos
+		pila_de_simbolos.pop();
+
+		/*
+		 * De acuerdo a la regla de producción de nuestra tabla la sustituimos
+		 */
+		pila_de_simbolos.add("a");
+
+		// Leemos el ultimo de la cola y el primero de la pila
+		ultimo_cola = cola.peek();
+		primero_pila = pila_de_simbolos.peek();
+
+		System.out.println(pila_de_simbolos);
+	}
+
+	private void reglaB() {
+		// Eliminamos primer elemento de la pila de simbolos
+		pila_de_simbolos.pop();
+
+		/*
+		 * De acuerdo a la regla de producción de nuestra tabla la sustituimos
+		 */
+		pila_de_simbolos.add("d");
+		pila_de_simbolos.add("C");
+		pila_de_simbolos.add("b");
+
+		// Leemos el ultimo de la cola y el primero de la pila
+		ultimo_cola = cola.peek();
+		primero_pila = pila_de_simbolos.peek();
+
+		System.out.println(pila_de_simbolos);
+	}
+
+	private void reglaC() {
+		// Eliminamos primer elemento de la pila de simbolos
+		pila_de_simbolos.pop();
+
+		/*
+		 * De acuerdo a la regla de producción de nuestra tabla la sustituimos
+		 */
+		pila_de_simbolos.add("c");
+
+		// Leemos el ultimo de la cola y el primero de la pila
+		ultimo_cola = cola.peek();
+		primero_pila = pila_de_simbolos.peek();
+
+		System.out.println(pila_de_simbolos);
+	}
+
+	private void lambda() {
+		// Eliminamos primer elemento de la pila de simbolos
+		pila_de_simbolos.pop();
+
+		/*
+		 * De acuerdo a la regla de producción de nuestra tabla la sustituimos
+		 */
+		// No agregamos nada por lambda
+
+		// Leemos el ultimo de la cola y el primero de la pila
+		ultimo_cola = cola.peek();
+		primero_pila = pila_de_simbolos.peek();
+
+		System.out.println(pila_de_simbolos);
+
+		// Regresamos
+		return;
+	}
+
+	private void consumir() {
+		// Removemos el simbolo en común tanto en cola como en pila
+		pila_de_simbolos.pop();
+		cola.remove();
+
+		// Leemos el ultimo de la cola y el primero de la pila
+		ultimo_cola = cola.peek();
+		primero_pila = pila_de_simbolos.peek();
+
+		System.out.println(pila_de_simbolos);
+
+		return;
+	}
+
+	private void insertarCola() throws ErrorSintactico {
+		while (tokens.getLexema() != Sym.PUNTOCOMA) {
+			if (tokens.getLexema() == Sym.A) {
+				cola.add("a");
+				siguienteToken();
+			}
+			if (tokens.getLexema() == Sym.B) {
+				cola.add("b");
+				siguienteToken();
+			}
+			if (tokens.getLexema() == Sym.C) {
+				cola.add("c");
+				siguienteToken();
+			}
+			if (tokens.getLexema() == Sym.D) {
+				cola.add("d");
+				siguienteToken();
+			}
+			if (tokens.getLexema() == Sym.PUNTOCOMA) {
+				cola.add(";");
+			}
+		}
+	}
+
+	private void siguienteToken() {
+		try {
+			tokens = analizador.yylex();
+			if (tokens == null) {
+				tokens = new Tokens("EOF", Sym.EOF, 0, 0);
+				throw new IOException("Fin de archivo");
+			}
+		} catch (IOException ex) {
+			System.out.println(ex.getMessage());
+		}
+
+	}
 
 }
